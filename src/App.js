fcc90d0932d68e61d21d47e6045cca6185925b17
Ayush Srivastava <114403598+Ayush6944@users.noreply.github.com>
2023-12-24 12:29:10 +0530

    git commit --signoff
    Signed-off-by: Ayush Srivastava <114403598+Ayush6944@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import styled, { ThemeConsumer, ThemeProvider } from 'styled-components'
+import { darkTheme } from './utils/Theme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Body=styled.div`
+background-color:${({theme })=>theme.bg};
+width:100%;
+overflow-x:hidden;
+`;
+
+function App(){
+  return(
+    <ThemeProvider theme={darkTheme}>
+      <Body></Body>
+    </ThemeProvider>
+  )
 }
-
-export default App;
