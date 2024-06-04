859a1c7a5a8f7726f8df6f5280375915ad713475
Ayush Srivastava <114403598+Ayush6944@users.noreply.github.com>
2024-06-04 13:47:42 +0530

    commit all

---
+++ src/components/Contact/index.js
@@ -0,0 +1,185 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { Snackbar, IconButton } from '@mui/material';
+import WhatsApp from '@mui/icons-material/WhatsApp';
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+position: relative;
+z-index: 1;
+align-items: center;
+@media (max-width: 960px) {
+    padding: 0px;
+}
+`
+
+const Wrapper = styled.div`
+position: relative;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: column;
+width: 100%;
+max-width: 1350px;
+padding: 0px 0px 80px 0px;
+gap: 12px;
+@media (max-width: 960px) {
+    flex-direction: column;
+}
+`
+
+const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+`;
+
+
+const ContactForm = styled.form`
+  width: 95%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.card};
+  padding: 32px;
+  border-radius: 16px;
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  margin-top: 28px;
+  gap: 12px;
+`
+
+const ContactTitle = styled.div`
+  font-size: 24px;
+  margin-bottom: 6px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary};
+`
+
+const ContactInput = styled.input`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`
+
+const ContactInputMessage = styled.textarea`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`
+
+const ContactButton = styled.input`
+  width: 100%;
+  text-decoration: none;
+  text-align: center;
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  padding: 13px 16px;
+  margin-top: 2px;
+  border-radius: 12px;
+  border: none;
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 18px;
+  font-weight: 600;
+`
+
+
+
+const Contact = () => {
+
+  //hooks
+  const [open, setOpen] = React.useState(false);
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+      .then((result) => {
+        setOpen(true);
+        form.current.reset();
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+const handlewhatsapp = () => {
+  window.open("https://wa.me/6386063411?text=Hello%20there%20I%20am%20interested%20in%20your%20services", "_blank");
+}
+
+
+  return (
+    <Container id='contact'>
+      <Wrapper>
+        <Title>Contact</Title>
+        <h3 style={{ color: '#fff' }}>You Can Contact through Whatsapp .. <span>
+          <IconButton aria-label="" onClick={handlewhatsapp}>
+            <WhatsApp fontSize='large' color='green' style={{ color: 'green' }} />
+          </IconButton> 
+          
+           </span></h3>
+        <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
+       
+       
+        
+        
+
+        <ContactForm ref={form} action='https://formspree.io/f/xayrwvbv' method='POST' >
+          <ContactTitle>Email Me 🚀</ContactTitle>
+          <ContactInput placeholder="Your Email" name="from_email" />
+          <ContactInput placeholder="Your Name" name="from_name" />
+          <ContactInput placeholder="Subject" name="subject" />
+          <ContactInputMessage placeholder="Message" rows="4" name="message" />
+          <ContactButton type="submit" value="Send IT " />
+        </ContactForm>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={()=>setOpen(false)}
+          message="Email sent successfully!"
+          severity="success"
+        />
+        <br/>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7014.329107805524!2d77.4988772800837!3d28.474590603738555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m3!3e6!4m0!4m0!5e0!3m2!1sen!2sin!4v1703412353723!5m2!1sen!2sin" width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Contact
---
+++ src/data/constants.js
@@ -0,0 +1,365 @@
+import buykart  from '../images/Buykart.png'
+import product from '../images/productdesigner.png'
+import pass from '../images/password.png'
+import resume from '../images/resume.png'
+import todo from '../images/todo.png'
+import togood from '../images/togood.png'
+import { WhatsApp } from '@mui/icons-material'
+
+
+export const Bio = {
+    name: "Ayush Srivastava",
+    roles: [
+      "Full Stack Developer",
+      "UI/UX Designer",
+      "Programmer",
+      "Coder",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Ayush6944",
+    resume:
+      "https://drive.google.com/file/d/1sVa4q6tPDbCEm4cgOBLBHe_QARVi4_Bh/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/ayushsrivastavaji/",
+    WhatsApp: "https://wa.me/6386063411?text=Hello%20Ayush",
+    insta: "https://www.instagram.com/ayushsrivastava6944/",
+    facebook: "https://www.facebook.com/profile.php?id=100015528425758&mibextid=ZbWKwL",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        }
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "firebase",
+          image: "https://th.bing.com/th/id/OIP.tq6KHIILzkMFuJDzjOdyIQHaHa?w=154&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        },
+        
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      
+      ],
+    },
+    ,
+    {
+      title: "Others",
+      skills: [
+       
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://mms.businesswire.com/media/20200326005142/en/761650/21/postman-logo-vert-2018.jpg",
+        },
+        {
+          name:"Python",
+          image:
+          "https://th.bing.com/th?q=Python+Logo.png&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247"
+        },
+        {
+          name:"C",
+          image:
+          "https://imgs.search.brave.com/eSKlJV5P2F8M6CBTbA7nYdmY70Jp2GlFJrxKAL30dxY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/c2FtLXNvbHV0aW9u/cy5jb20vYmxvZy93/cC1jb250ZW50L3Vw/bG9hZHMvMjAxOS8w/Ny9pbWFnZS1yZWNv/Z25pdGlvbi1DLXBy/b2dyYW1taW5nLWxh/bmd1YWdlLmpwZw"
+        },
+        
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "DevOps & Fullstack Engineering Intern",
+      company: "Flipr Inovations Pvt. Ltd.",
+      date: "Aug 2023 - July 2023",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      skills: [
+        "Docker",
+        "Terraform",
+        "AWS",
+        "EC2",
+        "Portainer",
+    
+        "JavaScript",
+        "TypeScript",
+        "Node Js",
+        " Next Js",
+      ],
+      doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+      role: "Frontend Engineer Intern",
+      company: "Neurobit",
+      date: "June 2023 - Present",
+      desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "Fullstack Externship",
+      company: "Flipr",
+      date: "June 2023 - July 2023",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Docker",
+        "AWS",
+        "MongoDB",
+      ],
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+      role: "Open Source Contributor ",
+      company: "GirlScript Summer of Code",
+      date: "May 2023 - Present",
+      desc: "Contributing to different open-source projects and learn from industry experts",
+      doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+    },
+    {
+      id: 3,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+      role: "Android Developer",
+      company: "DSC KIIT",
+      date: "Nov2021 - Present",
+      desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+      skills: [
+        "Leadership",
+        "Mobile Application Development",
+        "Kotlin",
+        "XML",
+        "Figma",
+      ],
+    },
+    {
+      id: 4,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+      role: "Android Developer Intern",
+      company: "Rudraksha Welffare Foundation",
+      date: "June 2021 - Oct 2021",
+      desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+      skills: [
+        "Android",
+        "Java",
+        "Kotlin",
+        "XML",
+        "Node Js",
+        "Cloud Firestore",
+        "Firebase",
+        "Figma",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+    },
+  ];
+  // import HeroImg from '../../images/HeroImage.jpg'
+
+  export const education = [
+    {
+      id: 0,
+      img: "https://imgs.search.brave.com/7gaGclfu0HwiPpKifv0dn_s9Pnm-hzFcci7PyDs3lsg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5jb2xsZWdlZGVr/aG8uY29tL21lZGlh/L2ltZy9pbnN0aXR1/dGUvbG9nby9pY2V0/cnlyLnBuZz93aWR0/aD05Ng",
+      school: "AKTU (formaly UPTU)",
+      date: "Oct 2021 - Sep 2025",
+      grade: "7.3 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at IEC Noida. I have completed 4 semesters and have a CGPA of 7.5. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at devtown, where I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://th.bing.com/th/id/OIP.oDlUJwsNZEcqw_JuaGVm5AHaJQ?w=96&h=120&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      school: "Avadh Collegiate(CBSE), Lucknow,UP ",
+      date: "Apr 2019 - Apr 2021",
+      grade: "85%",
+      desc: "I completed my class 12 high school education at AVADH  School, lko, where I studied Science with Computer Science.",
+      degree: "CBSE(XII), Science with Computer",
+    },
+    {
+      id: 2,
+      img: "https://th.bing.com/th/id/OIP.oDlUJwsNZEcqw_JuaGVm5AHaJQ?w=96&h=120&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      school: "Avadh collegiate, Lucknow",
+      date: "Apr 2017 - Apr 2019",
+      grade: "92.3%",
+      desc: "I completed my class 10 education at where I studied Science with Computer Application.",
+      degree: "CBSE(X), Science with Computer",
+    },
+  ];
+  
+  export const projects = [
+  
+    {
+      id: 0,
+      title: "E-commerece (BUYKART)",
+      date: "Apr 2023 - May 2023",
+      description:
+        "Developed a full-stack web application that allows users to make buy item and pay online  .",
+      image :  buykart ,
+           tags: ["React Js", "MongoDb", "Node Js", "Express Js"],
+      category: "web app",
+      github: "https://github.com/Ayush6944",
+      webapp: "https://65677b98defcab00a753633f--silly-faun-f0e2b8.netlify.app/",
+    
+    },
+    {
+      id: 1,
+      title: "RESUME BUILDER",
+      date: "Oct 2022 - Present",
+      description:
+        "Designed and developed the  project, Where people can develop resume using a templates and download in pdf formate using many different templates .",
+      image:resume,
+           tags: [
+        "React Js",
+        "MongoDb",
+        "Node Js",
+        "Express Js",
+        "Redux",
+        "NodeMailer",
+      ],
+      category: "web app",
+      github: "https://github.com/Ayush6944/Resume_builder",
+      webapp: "https://github.com/Ayush6944/Resume_builder",
+    },
+    // import HeroImg from '../../images/HeroImage.jpg'
+
+    {
+      id: 10,
+      title: "Clone Website (TOGOOD)",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "A clone website is here ,it is A award wining website ,which i have try to clone using basic mern stack.",
+      image: togood ,
+      tags: ["React Native", "JavaScript", "Axios"],
+      category: "android app",
+      github: "https://github.com/Ayush6944/cssanimation01/tree/main/project",
+      webapp: "https://cssanimation01.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Product Designer",
+      date: "Jul 2021",
+      description:
+        "A Rayben clone made with React JS. It has Phone Authentication, Real-time Database. good animation used sheryjs .",
+      image:
+        product,
+      tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+      category: "web app",
+      github: "https://github.com/Ayush6944/",
+      webapp: "https://ayush6944.github.io/website--1/",
+    },
+    {
+      id: 5,
+      title: "Password Genetrator",
+      date: "sep 2021",
+      description:
+        " A simple web ,used to show css skills .totally pure css is used to show effects (credit to- sheriyansh YT).",
+      image:pass
+        ,
+      tags: ["React Js", "CSS3","JS", "Node JS"],
+      category: "web app",
+      github: "https://github.com/Ayush6944/",
+      webapp: "https://github.com/Ayush6944/",
+    },
+    
+    {
+      id: 6,
+      title: "TO-DO List",
+      date: "Jul 2021",
+      description:
+        " A simple web is used to make a todo list online.",
+      image:
+        todo,
+      tags: ["React Js", "CSS3","JS", "Node JS"],
+      category: "web app",
+      github: "https://github.com/Ayush6944/todo-list.git",
+      webapp: "https://github.com/Ayush6944/todo-list.git",
+    },
+    
+  
+    
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
